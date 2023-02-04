be01d281ef918784eec3337cabdf62e56fa610d6
Joe Hartley <joejohnhartley@gmail.com>
2023-02-04 17:46:23 +0000

    adds new fields in the sign up and edit form partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :bio])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :date_of_birth])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio])
   end
 end
