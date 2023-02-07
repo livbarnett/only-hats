c5de72969bbb7ea94ce72ffda00ef62d6bdc7e79
Joe Hartley <joejohnhartley@gmail.com>
2023-02-07 21:52:09 +0000

    Adds review model along with its create route and a form partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :hats, only: %i[index show]
+  resources :bookings do
+    resources :reviews, only: %i[create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
