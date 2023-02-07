bb50e0e7595e5ab2bdd15a063598b8cd132cebd5
Lola Boatwright <lolaboatwright@gmail.com>
2023-02-07 21:34:31 +0000

    add bookings to pages controller profile view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "pages/profile", to: "pages#profile"
+
+  resources :bookings, except: [:show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
