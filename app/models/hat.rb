908013d14c2bf8042b0957db81c7d1e130f3e9d5
Joe Hartley <joejohnhartley@gmail.com>
2023-02-08 11:02:13 +0000

    Uses Cloudinary to allow uploading images for hats. Adds new, edit and create routes to hats to allow team to add hats.

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -1,6 +1,6 @@
 class Hat < ApplicationRecord
   self.inheritance_column = :not_type
-  # has_one_attached :photo
+  has_one_attached :photo
   has_many :bookings
   has_many :users, through: :bookings
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :hats, only: %i[index show]
+  resources :hats, only: %i[new create edit update index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
