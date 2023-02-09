278c66aae0c80f57a2b8e8f7354ac12d84d27cd5
Joe Hartley <joejohnhartley@gmail.com>
2023-02-09 21:42:49 +0000

    adds new booking form to the hat show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,24 @@
 class BookingsController < ApplicationController
-  def index
+  before_action :set_hat, only: %i[create]
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.hat = @hat
+    if @booking.save
+      redirect_to hat_path(@hat), notice: "Booking request was successfully made."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_hat
+    @hat = Hat.find(params[:hat_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:name, :start_date, :end_date)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :hat
+  validates_uniqueness_of :hat_id, scope: %i[start_date end_date]
 end
