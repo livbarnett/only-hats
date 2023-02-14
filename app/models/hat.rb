6541e48102fe8f918dd830aced8080c75574c2dd
Lola Boatwright <lolaboatwright@gmail.com>
2023-02-14 18:12:34 +0000

    add pg_search gem

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -1,7 +1,14 @@
 class Hat < ApplicationRecord
+  include PgSearch::Model
+
   self.inheritance_column = :not_type
   has_one_attached :photo
   has_many :bookings
   has_many :users, through: :bookings
 
+  pg_search_scope :search_by_name_and_type,
+  against: [ :name, :type ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
