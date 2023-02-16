13be6a5f9413be2b47d606c56eee09d7c0cf3e49
Joe Hartley <joejohnhartley@gmail.com>
2023-02-16 21:22:16 +0000

    adds ruby logic to booking index to show total price of each booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Hat.create(size: 56, location: "New York", type: "Fedora", color: "Black", description: "A classic black fedora", day_rate: 15)
-Hat.create(size: 58, location: "London", type: "Bowler", color: "Brown", description: "A stylish brown bowler", day_rate: 20)
-Hat.create(size: 60, location: "Paris", type: "Top Hat", color: "Black", description: "A formal black top hat", day_rate: 25)
